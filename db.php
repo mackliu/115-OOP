71edbfab3c91f82e02a50d239a705730ddfb23ac
mackliu <macklun@gmail.com>
2026-06-01 14:16:58 +0800

    建立物件導向應用DB上的第一個方法all()

--- db.php
+++ db.php
@@ -12,10 +12,30 @@ class DB{
     }
 
     function all(...$args){
+        $sql = "SELECT * FROM $this->table ";
+        //$sql = "SELECT * FROM $this->table";  //取得全部資料
+        //$sql = "SELECT * FROM $this->table WHERE `code`='A01' AND `name`='John'"; //取得符合條件的資料
+        //$sql = "SELECT * FROM $this->table LIMIT 10,10";    //取得第11筆到第20筆資料
+        //$sql = "SELECT * FROM $this->table WHERE `code`='A01' AND `name`='John' LIMIT 10,10";  //取得符合條件的資料中的第11筆到第20筆資料
 
-        $rows=$this->pdo->query("SELECT * FROM $this->table")->fetchAll(PDO::FETCH_ASSOC);
+            if(isset($args[0])){
+                if(is_array($args[0])){
+                    $tmp=[];
+                    foreach($args[0] as $idx => $val){
+                        $tmp[]="`$idx`='$val'";
+                    }
+                    $sql .= " WHERE ".join(" AND ",$tmp);
+                }else{
+                    $sql .=$args[0];
+                }
+            }
 
-        return $rows; //整個$table 的資料
+            if(isset($args[1])){
+                $sql .=" ".$args[1];
+            }
+          
+            echo $sql;
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
     }
 }
@@ -26,7 +46,15 @@ echo "<pre>";
 print_r($Status->all());
 echo "</pre>";
 echo "<pre>";
-print_r($Scores->all());
+print_r($Scores->all(['score'=>64]));
+echo "</pre>";
+echo "<pre>";
+print_r($Scores->all(" LIMIT 3,5"));
+echo "</pre>";
+
+
+echo "<pre>";
+print_r($Scores->all("WHERE `score`>60","LIMIT 3,5"));
 echo "</pre>";
 
 ?>
